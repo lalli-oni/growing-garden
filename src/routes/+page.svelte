96afec5997cad56366c5a627dcad1b86dbf8a769
lalli-oni <lalli.oni@gmail.com>
2024-07-29 15:46:53 +0200

    Remove welcome message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,13 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Larus Thor - Digital garden</title>
+	<meta name="description" content="Larus Thor digital garden" />
 </svelte:head>
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
+		
 	</h1>
 
 	<h2>
@@ -39,21 +32,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
