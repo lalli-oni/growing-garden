d9142677f4a95b3bb2dbe4ef44565f0b3ec5f086
lalli-oni <lalli.oni@gmail.com>
2024-07-30 13:50:40 +0200

    Add about-app

---
+++ src/routes/about-app/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>About app</title>
+	<meta name="description" content="About app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About app</h1>
+
+	<p>
+		This is a digital garden, loosely structured wiki, musings & implementation sandbox.
+	</p>
+
+	<p>
+		Scaffolded using vercel & sveltekit.
+	</p>
+</div>
