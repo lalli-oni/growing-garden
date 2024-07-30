d43d68046bc0127a53d99aa1ac92cc532ef4c665
lalli-oni <lalli.oni@gmail.com>
2024-07-30 13:50:14 +0200

    Remove sverdle link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -20,9 +20,6 @@
 			<li aria-current={$page.url.pathname === '/about-me' ? 'page' : undefined}>
 				<a href="/about-me">About me</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
