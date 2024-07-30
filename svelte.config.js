793f6003bc93db19c046786bdfa2b3589de2d5b2
lalli-oni <lalli.oni@gmail.com>
2024-07-30 13:46:48 +0200

    Add mdsvex Include .md file extension to svelte add mdsvex as preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		mdsvex({
+			// The default mdsvex extension is .svx; this overrides that.
+			extensions: ['.md'],
+		}),
+	],
 	kit: {
 		adapter: adapter()
 	}
