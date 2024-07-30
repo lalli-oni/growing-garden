be1cac3fe816689c0be3c1adb184856e1767c779
lalli-oni <lalli.oni@gmail.com>
2024-07-30 13:11:56 +0200

    Replace footer with email

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<a href="mailto:lalli.oni@gmail.com">E-mail</a>
 	</footer>
 </div>
 
