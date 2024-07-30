7a1c230cbcaafdba839ddc70da631503a6992958
lalli-oni <lalli.oni@gmail.com>
2024-07-30 13:50:56 +0200

    Add article page Add article nushell-script article

---
+++ src/routes/article/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Articles</title>
+	<meta name="description" content="Articles" />
+</svelte:head>
+
+<div class="text-column">
+    Here are markdown articles.
+    <a class="dropdown-content" href="/article/nushell-wrapper-script">Nushell wrapper script</a>
+</div>
---
+++ src/routes/article/nushell-wrapper-script/+page.md
@@ -0,0 +1,9 @@
+# Writing nushell custom scripts
+
+Nushell is a great shell to work with.
+
+Writing a custom script is a breeze.
+
+```
+nushell code here
+```
