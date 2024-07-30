eb3f16b68faf6a9f14ea8715725347550db8b011
lalli-oni <lalli.oni@gmail.com>
2024-07-30 13:50:35 +0200

    Change about => about-me

---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+	<title>About me</title>
+	<meta name="description" content="About me" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About me</h1>
+
+	<p>
+		I'm 37, Icelandic living in Denmark.
+	</p>
+
+	<p>
+		Short 2 year technical education in computer science centered around Microsoft stack (C#, ASP.NET, SQL server)
+	</p>
+
+	<p>
+		A few years of fullstack as one man shadow IT. Among which I made
+	</p>
+	<ul>
+		<li>Designing and implementing Oracle DB data warehouse w/ Stored Procedures</li>
+		<li>C# CLI data pipeline chron job</li>
+		<li>Large (10+k rows) jquery data tables</li>
+		<li>ASP.NET WebAPI</li>
+		<li>Parser and transcompiler of a 90's ASCII PLC program</li>
+		<li>Printing of a real world 18 meter long network graph</li>
+	</ul>
+</div>
