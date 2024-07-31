30d1bf0d630566b60a98321232989c2d130d9837
lalli-oni <lalli.oni@gmail.com>
2024-07-31 12:32:28 +0200

    Add article component

---
+++ src/routes/article/[slug]/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	export let data
+    console.log(data)
+</script>
+
+<!-- SEO -->
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article>
+  <!-- Title -->
+	<hgroup>
+		<h1>{data.meta.title}</h1>
+		<p>Published at {data.meta.created}</p>
+		<p>Modified at {data.meta.updated}</p>
+	</hgroup>
+
+  <!-- Tags -->
+	<div class="tags">
+		{#each data.meta.categories as category}
+			<span>&num;{category}</span>
+		{/each}
+	</div>
+
+  <!-- Post -->
+	<div class="prose">
+		<svelte:component this={data.content} />
+	</div>
+</article>
+
+<style>
+	article {
+		max-inline-size: var(--size-content-3);
+		margin-inline: auto;
+	}
+
+	h1 {
+		text-transform: capitalize;
+	}
+
+	.tags {
+		display: flex;
+		gap: 1rem;
+	}
+
+	.tags > * {
+		padding: 0.2rem 0.3rem;
+		border-radius: 2rem;
+        color: var(--color-highlight-text)
+	}
+</style>
