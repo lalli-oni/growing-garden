cefedec0370aa03a86135e0e85f330aed2d13f6b
lalli-oni <lalli.oni@gmail.com>
2024-08-01 15:16:47 +0200

    Add /experiments page

---
+++ src/routes/experiments/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+
+<div>
+    <p>
+        Placeholder for various experiments.
+    </p>
+
+    <ul>
+        <li><a href="experiments/wasm-rust">Compiling Rust into WebAssembly</a></li>
+        <li><a href="experiments/hex-grid-canvas">Hexagonal grid in canvas element</a></li>
+    </ul>
+</div>
+
+<style>
+</style>
