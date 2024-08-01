843a72dbd391948c92b827725b0665ca6b0f50ab
lalli-oni <lalli.oni@gmail.com>
2024-08-01 15:17:02 +0200

    Add rust wasm experiment

---
+++ src/routes/experiments/wasm-rust/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import * as wasm from "./pkg/frontier_rust.js"
+</script>
+
+
+
+<div>
+    <p>
+        Experimenting with integrating WASM compiled from Rust. This is loosely done with the idea of web app game development.
+    </p>
+
+    <p>
+        Used this guide to compile rust code into WebAssembly: https://rustwasm.github.io/docs/book/game-of-life
+    </p>
+
+    <p>
+        Copied the `/pkg` folder into this project. Don't want to bring in the complexity (yet!) of configuring the wasm compiling process into vite & sveltekit.
+    </p>
+
+    <h2>TODO</h2>
+    <ol>
+        <li>Look into database integration (use browser storage?)</li>
+        <li>Production bundle size</li>
+    </ol>
+
+    <button on:click={() => { wasm.greet() }}>Run wasm code</button>
+</div>
+
+<style>
+</style>
