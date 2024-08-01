c3c81f18219b0d205034909629bb8ec75d1a0c72
lalli-oni <lalli.oni@gmail.com>
2024-08-01 15:17:14 +0200

    Add Canvas component

---
+++ src/routes/experiments/hex-grid-canvas/Canvas.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	
+	export let width = 800
+	export let height = 500
+	export let color = '#333'
+	export let background = '#fff'
+	
+	let canvas
+	let context
+	
+    let corners
+
+    const angle = 2 * Math.PI / 6
+    const radius = 50
+	
+	onMount(() => {
+		context = canvas.getContext('2d')
+		//context.lineWidth = 3
+		
+		handleSize()
+
+        drawGrid()
+	})
+	
+	
+	const handleSize = () => {
+		const corners = canvas.getBoundingClientRect()
+	}
+
+    const drawGrid = () => {
+        for (let y = 0, j = 0; y < height; y += radius * Math.sin(angle)) {
+            for (
+                let x = 0;
+                x + radius * (1 + Math.cos(angle)) < width + 100;
+                x += radius * (1 + Math.cos(angle)), y += (-1) ** j++ * radius * Math.sin(angle)
+            ) {
+                drawHexagon(x, y);
+                console.log(`drawing hexagon at ${x}, ${y}. ${height}`)
+            }
+        }
+    }
+
+    const drawHexagon = (x, y) => {
+        context.beginPath();
+        for (var i = 0; i < 6; i++) {
+            context.lineTo(x + radius * Math.cos(angle * i), y + radius * Math.sin(angle * i));
+        }
+        context.closePath();
+        context.stroke();
+        context.fillStyle = "green"
+        context.fill()
+    }
+</script>
+
+<svelte:window on:resize={handleSize} />
+
+<canvas
+    {width}
+    {height}
+    style:background
+    bind:this={canvas}
+/>
