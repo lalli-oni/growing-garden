7cf8edbf1f3164047ba8e6e76ea2c35aae893c34
lalli-oni <lalli.oni@gmail.com>
2024-08-01 15:26:43 +0200

    Use wasm vite plugins

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		wasm(),
+    topLevelAwait()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
