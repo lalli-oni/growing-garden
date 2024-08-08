2fa0a721ab66dc4be716d38cb3025fdd62d018c7
lalli-oni <lalli.oni@gmail.com>
2024-08-08 11:58:02 +0200

    Add Controls component Dispatch selectHex event

---
+++ src/routes/experiments/hex-grid-canvas/Controls.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { Grid } from "./Grid";
+
+	import { createEventDispatcher } from 'svelte';
+
+    export let selectedHex: Hex
+    export let grid: Grid
+
+	const dispatch = createEventDispatcher<{ selectHex: Direction }>();
+</script>
+
+
+<div style="display: flex">
+    <button on:click={() => {
+        dispatch('selectHex', 'UpLeft')
+    }}>
+        UpLeft
+    </button>
+    <button on:click={() => {
+        dispatch('selectHex', 'Up')
+    }}>
+        Up
+    </button>
+    <button on:click={() => {
+        dispatch('selectHex', 'UpRight')
+    }}>
+        UpRight
+    </button>
+</div>
+<div style="display: flex">
+    <button on:click={() => {
+        dispatch('selectHex', 'DownLeft')
+    }}>
+        DownLeft
+    </button>
+    <button on:click={() => {
+        dispatch('selectHex', 'Down')
+    }}>
+        Down
+    </button>
+    <button on:click={() => {
+        dispatch('selectHex', 'DownRight')
+    }}>
+        DownRight
+    </button>
+</div>
