c71425499baac739b404c50493ee966030bc5f6d
lalli-oni <lalli.oni@gmail.com>
2024-08-09 13:30:18 +0200

    [bug] ts import changes

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
-import { Article } from "$lib/types"
-import { PageLoad } from "./$types"
+import type { Article } from "$lib/types.ts"
+import type { Load } from "@sveltejs/kit"
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: Load = async ({ fetch }) => {
 	const response = await fetch('api/articles')
 	const articles: Article[] = await response.json()
 	return { articles }
