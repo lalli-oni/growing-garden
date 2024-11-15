1342529e0403d895739313fe41f928e82b254a6c
lalli-oni <lalli.oni@gmail.com>
2024-11-15 16:48:05 +0100

    Remove email footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { webVitals } from '$lib/vitals';
+
 	import Header from './Header.svelte';
+
 	import './styles.css';
 
 	/** @type {import('./$types').LayoutServerData} */
@@ -23,10 +25,6 @@
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<a href="mailto:lalli.oni@gmail.com">E-mail</a>
-	</footer>
 </div>
 
 <style>
@@ -40,28 +38,8 @@
 		flex: 1;
 		display: flex;
 		flex-direction: column;
-		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
