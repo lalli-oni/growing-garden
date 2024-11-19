d830420a52daeb2f58f63a6eaa6fd346bd05225f
lalli-oni <lalli.oni@gmail.com>
2024-11-19 12:37:23 +0100

    Remove margin on tile headers

--- src/components/tiles/AboutMe.svelte
+++ src/components/tiles/AboutMe.svelte
@@ -1,11 +1,16 @@
 <h2>Who wrote this crap?</h2>
 <p>
-    Just a borderline insane frontend dev looking for the next spot to apply my trade.
+	Just a borderline insane frontend dev looking for the next spot to apply my trade.
 </p>
 <a href="/about-me">More...</a>
 
 <style>
-    a {
-        text-align: right;
-    }
+	a {
+		text-align: right;
+	}
+
+	
+	h2 {
+		margin: 0px;
+	}
 </style>
--- src/components/tiles/Experiments.svelte
+++ src/components/tiles/Experiments.svelte
@@ -1,9 +1,19 @@
 <h2>Experiments</h2>
 <ol>
-    <li>
-        <a href="/experiments/hex-grid-canvas">Hex grid in canvas element</a>
-    </li>
-    <li>
-        <a href="/experiments/wasm-rust">Compiled Rust -&gt; WASM code</a>
-    </li>
+	<li>
+		<a href="/experiments/hex-grid-canvas">Hex grid in canvas element</a>
+	</li>
+	<li>
+		<a href="/experiments/wasm-rust">Compiled Rust -&gt; WASM code</a>
+	</li>
 </ol>
+
+<style>
+	a {
+		text-align: right;
+	}
+
+	h2 {
+		margin: 0px;
+	}
+</style>
