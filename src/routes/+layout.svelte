3229e6afabdd4bbc0fda42b0f178d16e32107f51
lalli-oni <lalli.oni@gmail.com>
2024-11-20 14:13:32 +0100

    Add new cloud illustration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,9 +20,16 @@
 <div class="app">
 	<Header />
 
+	<div class="cloud-lines-illustration illustration">
+		<img src="/cloud-lines-illustration.svg" alt="cloud lines illustration" />
+	</div>
+
 	<main>
 		<slot />
 	</main>
+	<div class="cloud-illustration illustration">
+		<img src="/cloud-illustration.svg" alt="cloud illustration" />
+	</div>
 </div>
 
 <style>
@@ -39,5 +46,24 @@
 		width: 100%;
 		margin: 0 auto;
 		box-sizing: border-box;
+		overflow: auto;
+	}
+
+	.illustration {
+		position: absolute;
+		z-index: -10;
+		overflow: hidden;
+		display: inline;
+	}
+	
+	.cloud-lines-illustration {
+		top: 0;
+		left: -100px;
+	}
+
+	.cloud-illustration {
+		bottom: -60px;
+		right: -130px;
+		transform: rotate(90deg);
 	}
 </style>
