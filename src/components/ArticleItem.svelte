0e54a65f4e7bc3d73d49d9d4e6b71b10671b68d2
lalli-oni <lalli.oni@gmail.com>
2024-12-09 15:52:07 +0100

    Extract <ArticleItem />

---
+++ src/components/ArticleItem.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { formatDate } from '$lib/dateTime'
+	import type { Article } from '$lib/types'
+
+    export let article: Article
+</script>
+
+
+<a href={'/article/' + article.slug} class="article-recent-link">
+    <li>
+        <div class="header">
+            <span class="title">{article.title}</span>
+        </div>
+        <div class="footer">
+            <p class="date">{formatDate(article.updated)}</p>
+        </div>
+    </li>
+</a>
+
+<style>
+	li {
+		padding: 0.6rem 0.7rem;
+		border-radius: 1.5rem;
+	}
+
+	.title {
+		font-size: 1.2rem;
+	}
+
+	.footer {
+		font-size: 0.8rem;
+	}
+</style>
--- src/components/tiles/MostRecentPosts.svelte
+++ src/components/tiles/MostRecentPosts.svelte
@@ -1,45 +1,24 @@
 <script lang="ts">
-	import type { Article } from '$lib/types';
+	import type { Article } from '$lib/types'
+	import ArticleItem from '../ArticleItem.svelte'
 
 	interface ArticlesData {
 		articles: Array<Article>;
 	}
 
-	export let data: ArticlesData;
+	export let data: ArticlesData
 </script>
 
 <h2>Most recent articles</h2>
 <nav>
 	<ol>
-		{#each data.articles as article}
-			<a href={'/article/' + article.slug} class="article-recent-link">
-				<li>
-					<div class="header">
-						<span class="title">{article.title}</span>
-					</div>
-					<div class="footer">
-						<p class="date">{article.created}</p>
-					</div>
-				</li>
-			</a>
+		{#each data.articles.sort((a, b) => new Date(a.updated).getTime() - new Date(b.updated).getTime()) as article}
+			<ArticleItem article={article} />
 		{/each}
 	</ol>
 </nav>
 
 <style>
-	.article-recent-link > li {
-		padding: 0.6rem 0.7rem;
-		border-radius: 1.5rem;
-	}
-
-	.article-recent-link .title {
-		font-size: 1.2rem;
-	}
-
-	.article-recent-link .footer {
-		font-size: 0.8rem;
-	}
-
 	h2 {
 		margin: 0px;
 	}
