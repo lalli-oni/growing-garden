3980f2505a4b04fcd55af370fa85d1c28777b454
lalli-oni <lalli.oni@gmail.com>
2024-12-12 11:59:17 +0100

    Add employ page

---
+++ src/routes/about-me/employ/+page.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+	<title>About me - Employ</title>
+	<meta name="description" content="Employ me" />
+</svelte:head>
+
+<div class="text-column">
+    <h1>Why should consider employing me?</h1>
+    <h2>Values</h2>
+    <p>Transparency is important to me. I have never seen the point of punishing someone for telling you something you don't want to hear. Much less when it is a valuable piece of feedback one can use to develop oneself.</p>
+    
+    <h2>Experience</h2>
+    <p>I have quite an eclectic background, but most relevant perhaps in terms of frontend engineering I have a lot of experience with data tables, dahsboards and charting.</p>
+    
+    <h2>Common recruitment questions</h2>
+    <p>WIP. Will gather some common questions I get. I hope it doesn't detract from you being able to ask me questions directly, I am sure you wish to see how I perform unprepared as well, it's good to understand well.</p>
+    
+    <h2>Curriculum Vitae's</h2>
+    <div class="cv-links">
+        <a href="\CV-LarusThor-30oct2024.pdf">Curriculum Vitae</a>
+        <a href="\CV-LarusThor-31oct2024-NoPhoto.pdf">Curriculum Vitae (without photo)</a>
+        <p>Feel free to reach out in case you wish a more printable version or without name or identifiable characteristics (for equal opportunities).</p>
+    </div>
+</div>
+
+<style>
+    .cv-links {
+        display: flex;
+        flex-direction: column;
+        gap: 0.4rem;
+    }
+</style>
