a8ab3d72934e1caa37ca4c9075caaa217807a78f
lalli-oni <lalli.oni@gmail.com>
2024-12-12 12:05:09 +0100

    Small content update to /employ

--- src/routes/about-me/employ/+page.svelte
+++ src/routes/about-me/employ/+page.svelte
@@ -7,9 +7,10 @@
     <h1>Why should consider employing me?</h1>
     <h2>Values</h2>
     <p>Transparency is important to me. I have never seen the point of punishing someone for telling you something you don't want to hear. Much less when it is a valuable piece of feedback one can use to develop oneself.</p>
+    <p>I ma not here to sell you a used car. I hope to be finding a place to work in for an extended period of time, as such it's important to see if it's a good fit. As such I am not interested in presenting anything other than an honest image of me.</p>
     
     <h2>Experience</h2>
-    <p>I have quite an eclectic background, but most relevant perhaps in terms of frontend engineering I have a lot of experience with data tables, dahsboards and charting.</p>
+    <p>I have quite an eclectic background, but most relevant perhaps in terms of frontend engineering I have a lot of experience with data tables, dashboards and charting.</p>
     
     <h2>Common recruitment questions</h2>
     <p>WIP. Will gather some common questions I get. I hope it doesn't detract from you being able to ask me questions directly, I am sure you wish to see how I perform unprepared as well, it's good to understand well.</p>
