fc1a9ff53968553879bf44beb6eeccd10345f159
lalli-oni <lalli.oni@gmail.com>
2025-08-31 14:39:55 +0200

    Add vite-plugin-wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
+import wasm from "vite-plugin-wasm";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), wasm(), sveltekit()]
 });
