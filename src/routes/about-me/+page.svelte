b7bc9688da83dd3e1208682d566084075903ccb7
lalli-oni <lalli.oni@gmail.com>
2025-09-04 12:35:52 +0200

    Update about me

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -7,15 +7,20 @@
 	<h1>About me</h1>
 
 	<p>
-		I'm 37, Icelandic living in Denmark.
+		Sprouted in Iceland. Favors the untrodden path, fascinated by the interesting (possibly a trait attributed to my boredom allergy, sometimes refered to as ADHD).
 	</p>
 
 	<p>
-		Short 2 year technical education in computer science centered around Microsoft stack (mainly C#, ASP.NET design patterns and agile).
+		I have trained ballroom & latin dancing for more than 10 years. Worked in factories. Studied filmmaking. Made hotel beds. Been thrown around as a zombie.
 	</p>
 
 	<p>
-		A few years of fullstack as one man shadow IT. Among which I made
+		Sailed the Atlantic (well, I let the ferry crew handle most of it to be honest) down to Denmark.
+		Grabbed me some technical education (AP degree Datamatiker) computer science focused on the Microsoft stack.
+	</p>
+
+	<p>
+		Job history is sporadic but importantly gathered my chops early on as a fullstack consultant early on.
 	</p>
 	<ul>
 		<li>Designing and implementing Oracle DB data warehouse w/ Stored Procedures</li>
@@ -24,6 +29,15 @@
 		<li>ASP.NET WebAPI</li>
 		<li>Parser and transcompiler of a 90's ASCII PLC program</li>
 		<li>Printing of a real world 18 meter long network graph</li>
-		<li>vue product suite</li>
+		<li>Building vue product suite along with component library</li>
+	</ul>
+	<p>
+		As a frontend developer I have built my share of applications but a lot of my experience has been concerning building a frontend specialization in a fullstack team.
+	</p>
+	<ul>
+		<li>Developing frontend testing strategies</li>
+		<li>Establishing frontend ownership/responsibilities</li>
+		<li>Influence API design</li>
+		<li>Facilitate discussions on technology choices</li>
 	</ul>
 </div>
