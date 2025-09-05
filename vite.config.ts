7eaa413edab47cb08f528b195311eb82072d3512
lalli-oni <lalli.oni@gmail.com>
2025-09-05 12:52:37 +0200

    Add eslint rule no semicolons

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import wasm from "vite-plugin-wasm";
+import wasm from 'vite-plugin-wasm';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
