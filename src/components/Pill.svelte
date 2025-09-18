8354b2a1c07e147755c38cae48e04c0364ccdf53
lalli-oni <lalli.oni@gmail.com>
2025-09-18 13:27:43 +0200

    Add <Pill /> component

---
+++ src/components/Pill.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let label: string
+    // Hexadecimal color value for background
+    export let backgroundColor: string 
+    // Hexadecimal color value for foreground (default: white)
+    export let textColor: string = "#ffffff"
+</script>
+
+<span style:background-color={backgroundColor} style:color={textColor}>{label}</span>
+
+<style>
+    span {
+        padding: 0.3rem 0.5rem;
+        border-radius: 5rem;
+    }
+</style>
