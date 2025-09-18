98a9bbb0c3c94e0911d85fe38b7cb92a11b3a423
lalli-oni <lalli.oni@gmail.com>
2025-09-18 13:35:36 +0200

    Add typing to article slug data

--- src/routes/articles/[slug]/+page.ts
+++ src/routes/articles/[slug]/+page.ts
@@ -1,14 +1,34 @@
 import { error } from '@sveltejs/kit'
 import type { PageLoad } from './$types'
 
-export const load: PageLoad = async ({ params }) => {
+interface PageData {
+	content: () => any;
+	meta: ArticleMeta;
+}
+
+interface ArticleMeta {
+	title: string;
+	description: string;
+	categories: Array<string>;
+	published: boolean;
+	created: string;
+	updated: string;
+}
+
+export const load: PageLoad = async ({ params }): Promise<PageData> => {
 	try {
 		const post = await import(`../../../articles/${params.slug}.md`)
 
-		return {
+		const data: PageData = {
 			content: post.default,
 			meta: post.metadata
 		}
+
+		// NOTE (LTJ): What level of validation do we want?
+		if (!data.meta.published) error(404, `Could not find ${params.slug}`)
+		if (data.meta.title?.length < 1) error(501, `Missing article title for ${params.slug}`)
+
+		return data
 	} catch (e) {
 		error(404, `Could not find ${params.slug}`)
 	}
