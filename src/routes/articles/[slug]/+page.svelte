54442c6b9ecaad92b244439abeba47b1553518a9
lalli-oni <lalli.oni@gmail.com>
2025-10-02 10:37:10 +0200

    Improve article slugs Format meta dates Enhance spacing Make meta text more compact

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Pill from '../../../components/Pill.svelte'
+	import { formatDate } from '$lib/dateTime'
 
 	import type { PageProps } from './$types'
 
@@ -17,16 +18,16 @@
 	<!-- Title -->
 	<hgroup>
 		<h1>{data.meta.title}</h1>
-		<p>Published at {data.meta.created}</p>
-		<p>Modified at {data.meta.updated}</p>
+		<p>Published: {formatDate(data.meta.created)}</p>
+		<p>Modified: {formatDate(data.meta.updated)}</p>
+		<!-- Tags -->
+		<div class="tags">
+			{#each data.meta.categories as category (category)}
+				<Pill label={category} backgroundColor="#CC5500" />
+			{/each}
+		</div>
 	</hgroup>
 
-	<!-- Tags -->
-	<div class="tags">
-		{#each data.meta.categories as category (category)}
-			<Pill label={category} backgroundColor="#CC5500" />
-		{/each}
-	</div>
 
 	<!-- Post -->
 	<div class="prose">
@@ -38,13 +39,14 @@
 <style>
 	article {
 		padding: 1rem;
-		padding-top: 2rem;
+		padding-top: 4rem;
 		max-inline-size: var(--size-content-3);
 		margin-inline: auto;
 	}
 
 	h1 {
 		text-transform: capitalize;
+		margin: 0rem;
 	}
 
 	.tags {
@@ -56,5 +58,16 @@
 		padding: 0.2rem 0.3rem;
 		border-radius: 2rem;
 		color: var(--color-highlight-text);
+	}
+
+	hgroup {
+		/* font-weight: 300; */
+		padding-bottom: 1rem;
+	}
+
+	hgroup > p {
+		color: white;
+		font-size: 0.9rem;
+		margin: 0.4rem 0rem;
 	}
 </style>
